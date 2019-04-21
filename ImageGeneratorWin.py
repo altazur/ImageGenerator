cb37ad335938afc0d4be7bfe18c06bda553cf324
altazur <altazur.91@gmail.com>
2019-04-21 23:53:05 +0300

    Fix issue when 1MPX or 1MB generation wasn't possible (throwed an error) Added two constants to use less 'magical numbers' Fix some comments

--- ImageGeneratorWin.py
+++ ImageGeneratorWin.py
@@ -6,6 +6,10 @@
 from PIL import Image
 import argparse
 
+#CONSTANTS
+WEIGHT_MPX_GENER = 3000
+IMG_1MB_SIDE = 500
+
 def main(inp):
 	if (args.mb is not None):
 		print("***Creating image with {} MB***".format(inp))
@@ -15,18 +19,19 @@ def main(inp):
 		print("***Creating image with {}  MPX***".format(inp))
 		im = Image.new("RGB", GetImageSizeForMpx(args.mpx),0)
 		im.save("image"+str(args.mpx)+"mpx.jpg")
+	else: print("Empty argument")
 
 #Method to obtain desirable MPX image
 #We search if the mpx can be obtain by multiply one number (like 1000x1000). If not we just setting width to 3000
 def GetImageSizeForMpx(n):
     p = n
     for x in range(0,n):
-        p = p-1
         if ((p*p) == n):
             return (p*1000,p*1000)
             break
-        elif (p==1):
-            return (3000, int(n*1000000/3000))
+        elif (p==0): #change this from "1" to "0" to make 1mpx generation possible
+            return (WEIGHT_MPX_GENER, int(n*1000000/WEIGHT_MPX_GENER))
+	p -= 1 #cycle increment went here to make 1mpx generation possible
 
 #Because 1mpx of RGBA white image without compression is 4 MB file size
 #Method GetImageSize check whether the input is the exponent of 4
@@ -34,6 +39,7 @@ def GetImageSizeForMpx(n):
 #If no method returnes image which has 500px X (output*500)px file size which is usable when output can't be divided by 4 or at least 2
 #THe algorithm should be improved by finding the way to create the more than 500 px side images for non exponents of 4 sizes
 def GetImageSizeForMb(mb):
+	if (mb==1): return (500,500) #horrible but effective workaround for 1mb generation. I have SystemError: Tile cannot extend outside image error
 	tmp = 1
 	count = 0
 	while tmp < mb:
@@ -41,7 +47,7 @@ def GetImageSizeForMb(mb):
 		count += 1;
 	if (tmp == mb):
 		return (1000*count,1000*count)
-	return (500,mb*500)
+	return (IMG_1MB_SIDE,mb*IMG_1MB_SIDE)
  
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description="Generate MB driven or MPX driven image file")
@@ -53,4 +59,4 @@ if __name__=="__main__":
 	elif args.mpx:
 		main(args.mpx)
 else:
-	print("Error: the right format is '-mb 1' or '-mpx 1'")	
+	print("Error: the right format is '--mb 1' or '--mpx 1'")	
